73356139b475c4e0d7918eb1471247d363462d83
bernardo.araujo1612 <bernardo.araujo1612@gmail.com>
2026-03-09 20:14:58 +0000

    agora vai

---
+++ database/migrations/2026_03_09_171352_change_happened_at_to_nullable_visits_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('visits', function (Blueprint $table) {
+            $table->date('happened_at')->nullable()->change();
+            DB::statement("UPDATE visits SET happened_at = NULL WHERE happened_at < '2000-01-01'");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('nullable_visits', function (Blueprint $table) {
+            $table->date('happened_at')->nullable(false)->change();
+        });
+    }
+};
