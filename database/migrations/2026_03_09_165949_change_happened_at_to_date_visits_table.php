1fef78d358f04dead0fd138f47845a190ab64182
bernardo.araujo1612 <bernardo.araujo1612@gmail.com>
2026-03-09 20:02:26 +0000

    aiaiaiaiie

--- database/migrations/2026_03_09_165949_change_happened_at_to_date_visits_table.php
+++ database/migrations/2026_03_09_165949_change_happened_at_to_date_visits_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('date_visits', function (Blueprint $table) {
+        Schema::table('happened_at', function (Blueprint $table) {
             //
         });
     }
