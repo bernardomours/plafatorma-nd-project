7f318ce84f64a09824eb631886022ef8cf556c23
bernardo.araujo1612 <bernardo.araujo1612@gmail.com>
2026-03-13 20:03:21 +0000

    liberando acesso visitas

--- app/Filament/Resources/Visits/VisitResource.php
+++ app/Filament/Resources/Visits/VisitResource.php
@@ -85,29 +85,11 @@ class VisitResource extends Resource
         });
     }
 
-    // public static function canViewAny(): bool
-    // {
-    //     return true;
-    // }
-
     public static function canViewAny(): bool
     {
-        $user = auth()->user();
-
-        if ($user?->is_admin) {
-            return true;
-        }
-
-        $profissional = \App\Models\Professional::withoutGlobalScopes()
-                            ->where('email', $user->email)
-                            ->first();
-
-        if ($profissional && in_array($profissional->role->value, ['coordinator', 'supervisor'])) {
-            return false; 
-        }
-        
         return true;
     }
+
     /**
      * @return array<int, class-string|string>|
      */
